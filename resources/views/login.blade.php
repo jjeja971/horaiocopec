7bf2409aacbc7a376b1efcaa8180accc51ea49e3
felipe herrera <pipeherreguz@gmail.com>
2020-11-12 01:41:24 -0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,6 +14,8 @@
 
 </head>
 <body>
+
+<div class="content-header">
     <div class="container-fluid">
         <div class="row">
         
@@ -30,34 +32,43 @@
                     <div class="card-body">
                         <h2 class="mt-4 card-text">
                             Registro de usuario.
-                          </h2>
+                        </h2>
                         <div class="form-group">
                             <label class="mt-5">Nombre de usuario</label>
                             <input class="mb-4 form-control" placeholder="Ingrese nombre usuario">
                             
-                          </div>
-                          <div class="mb-5 form-group">
+                        </div>
+                        <div class="mb-5 form-group">
                             <label>Contraseña</label>
                             <input type="password" class="form-control" placeholder="Contraseña">
-                          </div>
+                        </div>
                     </div>
-                  </div><!-- /.card -->
+                </div><!-- /.card -->
             </div>
             <div class="col-lg-4"></div>
- 
+
             <div class="col-lg-4"></div>
             <div class="col-lg-4"  style="text-align: center">
                 <a href="." class="mt-4 btn btn-primary btn-lg btn-block">Iniciar sesion</a>
             </div>
             <div class="col-lg-4"></div>
-
-
-            
-
-
-
-
         </div>
     </div>
+   
+
+    <footer class="main-footer" style="bottom:1em; position: absolute; width:98%; height: 40px; margin:0">
+        <!-- To the right -->
+        <div class="float-right d-none d-sm-inline">
+          Software desarrollado por...
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2020 <a href="https://ww2.copec.cl/#/">Copec</a>.</strong> Todos los derechos reservados.
+      </footer>
+</div>
+<!-- jQuery -->
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
