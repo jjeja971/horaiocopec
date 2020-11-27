8493a8f5c66a75f3bc6d3ab6add81a990ae6291b
felipe herrera <pipeherreguz@gmail.com>
2020-11-27 11:40:32 -0300

    Update list_atendedores.blade.php

--- resources/views/atendedores/list_atendedores.blade.php
+++ resources/views/atendedores/list_atendedores.blade.php
@@ -1,51 +1,47 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
 @extends('layout')
 @section('content')
-
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
-
+<a href="nuevoatendedor" type="buttom" class="btn btn-success btn-lg">Agregar nuevo Atendedor</a>
+<hr>
 <div class="card">
-    <a href="nuevoatendedor" type="submit" class="btn btn-block btn-info btn-lg">Agregar Atendedor</a>
-      <div class="card-header">
-        <h3 class="card-title">DataTable with default features</h3>
-      </div>
-      <!-- /.card-header -->
-      <div class="card-body">
-        <table id="example" class="table table-bordered table-striped">
-          <thead>
+  <div class="card-header">
+    <h3 class="card-title">Lista de atendedores</h3>
+  </div>
+  <!-- /.card-header -->
+  <div class="card-body">
+    <table id="example" class="table table-bordered table-striped">
+      <thead>
+      <tr>
+        <th>Rut</th>
+        <th>Nombres</th>
+        <th>Telefono</th>
+        <th>Correo</th>
+        <th>Direccion</th>
+        <th>Editar</th>
+      </tr>
+      </thead>
+      <tbody>
+        @foreach ($lista as $item)
           <tr>
-            <th>Rendering engine</th>
-            <th>Browser</th>
-            <th>Platform(s)</th>
-            <th>Engine version</th>
-            <th>CSS grade</th>
-            <th></th>
+              <td>{{$item->rut_atendedor}}</td>
+              <td>{{$item->nombre_atendedor}}</td>
+              <td>{{$item->numero}}</td>
+              <td>{{$item->email}}</td>
+              <td>{{$item->direccion}}</td>
+              <td><a href="#"  class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Editar</a></td>
           </tr>
-          </thead>
-          <tbody>
-            @foreach ($lista as $item)
-            <tr>
-                <td>{{$item->rut_atendedor}}</td>
-                <td>{{$item->nombre_atendedor}}</td>
-                <td>{{$item->numero}}</td>
-                <td>{{$item->email}}</td>
-                <td>{{$item->direccion}}</td>
-                <td><a href="" class="btn btn-app"><i class="fas fa-edit"></i> Editar</a></td>
-            </tr>
-            @endforeach
-          </tbody>
-  
-        </table>
-      </div>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
       <!-- /.card-body -->
-    </div>
+</div>
   
 @endsection
 
   <script>
-      window.onload = function() {
-        $(document).ready(function() {
-            $('#example').DataTable();
-        } );
+      window.onload = function() { 
+          $('#example').DataTable(); 
       }
   </script>
   
