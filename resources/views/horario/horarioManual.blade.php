6ed851337bd9453adec311b8c77abb576a4252fc
felipe herrera <pipeherreguz@gmail.com>
2021-01-29 00:07:46 -0300

    Update horarioManual.blade.php

--- resources/views/horario/horarioManual.blade.php
+++ resources/views/horario/horarioManual.blade.php
@@ -113,28 +113,38 @@
                         <div class="modal-body">
                             <div class="container-fluid" style="text-align: center">
                                 <h3 style="color: #395ca8"> Seleccione el personal a asignar al turno elegido</h3>                              
-                                <div class="col-lg-12">
+                                <div class="col-lg-12 mt-5">
                                 
                                     <div class="row" >
                                         <div class="col-lg-3"></div>
                                         <div class="col-lg-3">
                                             @if (session("fecha_horario_m"))
                                                 <input id="date" type="date" name="date" max="3000-12-31" min="1000-01-01" 
-                                                style="font-size: 1.6em; color:#1d59a7" class="form-control" value="{{session("fecha_horario_m")}}"> 
+                                                style="font-size: 1.6em; color:#1d59a7" class="form-control" value="{{session("fecha_horario_m")}}" readonly> 
                                              @endif
                                         </div>
-                                        <div class="col-lg-3">
+                                        <div class="col-lg-3"> 
+                                            <select id="modturno" name="modturno" style="font-size: 1.6em; width:100%; color:#1d59a7"> 
+                                                @foreach ($turno as $item) 
+                                                    <option value="{{ $item->id_turno }}">{{ $item->hora_entrada }} - {{ $item->hora_salida }}</option>   
+                                                @endforeach  
+                                            </select>    
+                                        </div>
+                                        <div class="col-lg-3"></div>
+
+                                        <div class="col-lg-3"></div>
+                                        <div class="col-lg-6 mt-4">
                                             <select id="listapersonal" name="listapersonal" style="font-size: 1.6em; width:100%; color:#1d59a7"> 
                                                 @foreach ($personalrec as $item) 
                                                     <option value="{{ $item->rut_atendedor }}">{{ $item->nombre_atendedor }}</option>   
                                                 @endforeach  
-                                            </select>     
+                                            </select>  
                                         </div>
                                         <div class="col-lg-3"></div>
 
                                         <div class="col-lg-3"></div>
                                         <div class="col-lg-6">
-                                            <button type="submit" id="btnRemoverTurno" style="width: 16em;" class="btn btn-warning btn-lg mb-3 mt-5"><b>Remover este turno</b></button>
+                                            <button type="submit" id="btnRemoverTurno" style="width: 16em;" class="btn btn-danger btn-lg mb-3 mt-5"><b>Remover este turno</b></button>
                                         </div>
                                         <div class="col-lg-3"></div>
                                         
@@ -215,7 +225,12 @@ window.onload = function() {
         if($('#date').val()){
             @foreach($verificarFecha as $item2)
                 dataTable.addRows([
-                    [ "{{$item2->rut_atendedor}}", "{{$item2->nombre_atendedor}}", new Date(0, 0, 0, {{$item2->hora_entrada}}, {{$item2->min_entrada}}), new Date(0, 0, 0, {{$item2->hora_salida}}, {{$item2->min_salida}}) ]
+                    [ "{{$item2->rut_atendedor}}",
+                      "{{$item2->nombre_atendedor}}",
+                      new Date(0, 0, 0, {{$item2->hora_entrada}},
+                      {{$item2->min_entrada}}),
+                      new Date(0, 0, 0, {{$item2->hora_salida}},
+                      {{$item2->min_salida}}) ]
                 ]);
             @endforeach
             //DIBUJAR GRAFICO SOLO CUANDO EXISTAN 1 O MAS DATOS
