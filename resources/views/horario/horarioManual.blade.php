d61d34bcad2227fb8cfb3541d16e5069695c0d61
felipe herrera <pipeherreguz@gmail.com>
2021-01-28 23:54:33 -0300

    Update horarioManual.blade.php

--- resources/views/horario/horarioManual.blade.php
+++ resources/views/horario/horarioManual.blade.php
@@ -116,19 +116,28 @@
                                 <div class="col-lg-12">
                                 
                                     <div class="row" >
-                                        <div class="col-lg-4"></div>
-                                        <div class="col-lg-2">
-                                            
-         
+                                        <div class="col-lg-3"></div>
+                                        <div class="col-lg-3">
+                                            @if (session("fecha_horario_m"))
+                                                <input id="date" type="date" name="date" max="3000-12-31" min="1000-01-01" 
+                                                style="font-size: 1.6em; color:#1d59a7" class="form-control" value="{{session("fecha_horario_m")}}"> 
+                                             @endif
+                                        </div>
+                                        <div class="col-lg-3">
                                             <select id="listapersonal" name="listapersonal" style="font-size: 1.6em; width:100%; color:#1d59a7"> 
                                                 @foreach ($personalrec as $item) 
                                                     <option value="{{ $item->rut_atendedor }}">{{ $item->nombre_atendedor }}</option>   
                                                 @endforeach  
-                                            </select>    
-                                            <button type="submit" id="btnRemoverTurno" style="width: 16em;" class="btn btn-warning btn-lg mb-3"><b>Remover este turno</b></button>
-                                            
+                                            </select>     
                                         </div>
-                                        <div class="col-lg-2"></div>
+                                        <div class="col-lg-3"></div>
+
+                                        <div class="col-lg-3"></div>
+                                        <div class="col-lg-6">
+                                            <button type="submit" id="btnRemoverTurno" style="width: 16em;" class="btn btn-warning btn-lg mb-3 mt-5"><b>Remover este turno</b></button>
+                                        </div>
+                                        <div class="col-lg-3"></div>
+                                        
                                     </div>
                                 </div>
                             </div>
