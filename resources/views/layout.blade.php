8d63edd804232e21b1b03c0a5749abf2de1c1700
felipe herrera <pipeherreguz@gmail.com>
2021-02-22 15:07:37 -0300

    icon subir excel layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -129,7 +129,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li> 
           <li class="nav-item">
             <a href="/cargartransaccion" class="nav-link">
-              <i class="fas fa-users-cog nav-icon"></i>
+              <i class="fas fa-file-upload nav-icon"></i>
               <p>
                 Cargar Transacciones
               </p>
