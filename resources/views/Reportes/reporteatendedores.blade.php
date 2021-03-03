2836a47633ac7c3c2872f251aa7b12898a131d58
felipe herrera <pipeherreguz@gmail.com>
2021-03-03 13:10:28 -0300

    Update reporteatendedores.blade.php

--- resources/views/Reportes/reporteatendedores.blade.php
+++ resources/views/Reportes/reporteatendedores.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,25 +7,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    asd
-    <div id="invoice" name="invoice">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="card-body">
+        <table id="example" class="table table-bordered table-striped">
+          <thead>
+          <tr>
+            <th>Rut</th>
+            <th>Nombres</th>
+            <th>Telefono</th>
+            <th>Correo</th>
+            <th>Direccion</th>
+            <th>Editar</th>
+          </tr>
+          </thead>
+          <tbody>
             @foreach ($lista as $item)
-                <tr>
-                    <td>{{$item->rut_atendedor}}</td>
-                    <td>{{$item->nombre_atendedor}}</td>
-                </tr>
+              <tr>
+                <td>{{$item->rut_atendedor}}</td>
+                <td>{{$item->nombre_atendedor}}</td>
+                <td>{{$item->numero}}</td>
+                <td>{{$item->email}}</td>
+                <td>{{$item->id_jornada}}</td>
+              </tr>
             @endforeach
-        </tbody>
-    </table>
-</div>
+          </tbody>
+        </table>
+      </div>
 </body>
 </html>
